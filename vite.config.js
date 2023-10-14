1c39d6fc719e84c9230444365df17edb0856b1c7
Leonardo Aceituno <leonardo.aceituno@gmail.com>
2023-10-14 20:18:57 -0300

    Fix vite config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base:'https://leonardo-aceituno.github.io/weather-app/'
+  base:'https://leonardo-aceituno.github.io/weather-app/',
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/weather-app/'
+    : '/'
 })
